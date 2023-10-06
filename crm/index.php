019be8e7877a6a33188f1499810fdf84d71a9bfd
dheerajkharwar <dheerajkharwar9@gmail.com>
2023-10-06 19:37:27 +0530

    CRM Login added

--- crm/index.php
+++ crm/index.php
@@ -15,6 +15,7 @@ session_start();
     <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
     <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <link rel="stylesheet" href="index.css">
+</head>
 
 <body>
     <div id="login">
