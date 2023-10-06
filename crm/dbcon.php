99234d93050bd18a43cd2beef5048ed9537fb5be
dheerajkharwar <dheerajkharwar9@gmail.com>
2023-10-06 19:39:46 +0530

    CRM Login added

---
+++ crm/dbcon.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "localhost";
+$username = "u768844772_crm123";
+$password = "Crm@Mihu123";
+$dbname = "u768844772_crm_db";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+?>
