ad42887b52e9629a343ee532b50995e03b5f60b2
dheerajkharwar <dheerajkharwar9@gmail.com>
2023-10-06 18:43:15 +0530

    CRM Login added

--- crm/index.php
+++ crm/index.php
@@ -30,8 +30,6 @@
                                 <input type="text" name="password" id="password" class="form-control">
                             </div>
                             <div class="form-group">
-                                <label for="remember-me" class="text-info"><span>Remember me</span> <span><input
-                                            id="remember-me" name="remember-me" type="checkbox"></span></label><br>
                                 <input type="submit" name="submit" class="btn btn-info btn-md" value="submit">
                             </div>
                         </form>
