b9cb5afcc6289c536cfda25a57c768b1bb6ae746
dheerajkharwar <dheerajkharwar9@gmail.com>
2023-10-06 19:42:57 +0530

    CRM Login added

--- crm/index.php
+++ crm/index.php
@@ -11,9 +11,8 @@ session_start();
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>CRM - Company Name</title>
 
-    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
-    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+
     <link rel="stylesheet" href="index.css">
 </head>
 
