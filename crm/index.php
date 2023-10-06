fd79d8998708ede09316f04c2e4badb58695505e
dheerajkharwar <dheerajkharwar9@gmail.com>
2023-10-06 18:23:03 +0530

    CRM Login added

--- crm/index.php
+++ crm/index.php
@@ -25,7 +25,7 @@
   <body>
   <div class="container align-items-center">
     <h1 class="text-center">Login</h1>
-  <div class="col-md-4">
+  <div class="col-md-12">
   <form>
   <!-- Email input -->
   <div class="form-outline mb-4">
