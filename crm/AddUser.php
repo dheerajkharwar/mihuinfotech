a5a9f260650314eadc739cbb5086f537209b30de
dheerajkharwar <dheerajkharwar9@gmail.com>
2023-10-07 17:14:00 +0530

    Version 1.1 27/09/23 Ready for production

---
+++ crm/AddUser.php
@@ -0,0 +1,60 @@
+<?php 
+include "dbcon.php";
+session_start();
+if(!isset($_SESSION['admin'])){
+    header('Location: admin.php');
+    exit;
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8"/>
+    <title>Add User</title>
+    <link rel="stylesheet" href="style.css"/>
+</head>
+<body>
+<?php
+    require('db.php');
+    // When form submitted, insert values into the database.
+    if (isset($_REQUEST['username'])) {
+        // removes backslashes
+        $username = stripslashes($_REQUEST['username']);
+        //escapes special characters in a string
+        $username = mysqli_real_escape_string($con, $username);
+        $name = stripslashes($_REQUEST['name']);
+        $name = mysqli_real_escape_string($con, $name);
+        $email    = stripslashes($_REQUEST['email']);
+        $email    = mysqli_real_escape_string($con, $email);
+        $password = stripslashes($_REQUEST['password']);
+        $password = mysqli_real_escape_string($con, $password);
+        $create_datetime = date("Y-m-d H:i:s");
+        $query    = "INSERT into `user` (username, name, email, password)
+                     VALUES ('$username', '$name', '$email', '" . md5($password) . "')";
+        $result   = mysqli_query($con, $query);
+        if ($result) {
+            echo "<div class='form'>
+                  <h3>You are registered successfully.</h3><br/>
+                  <p class='link'>Click here to <a href='login.php'>Login</a></p>
+                  </div>";
+        } else {
+            echo "<div class='form'>
+                  <h3>Required fields are missing.</h3><br/>
+                  <p class='link'>Click here to <a href='registration.php'>registration</a> again.</p>
+                  </div>";
+        }
+    } else {
+?>
+    <form class="form" action="" method="post">
+        <h1 class="login-title">Registration</h1>
+        <input type="text" class="login-input" name="username" placeholder="Username" required />
+        <input type="text" class="login-input" name="name" placeholder="Name" required />
+        <input type="text" class="login-input" name="email" placeholder="Email Adress" required>
+        <input type="password" class="login-input" name="password" placeholder="Password" required>
+        <input type="submit" name="submit" value="Register" class="login-button">
+    </form>
+<?php
+    }
+?>
+</body>
+</html>
---
+++ crm/user.php
@@ -0,0 +1,63 @@
+
+<?php 
+include "dbcon.php";
+session_start();
+if(!isset($_SESSION['email']) || !isset($_SESSION['user'])){
+    header('Location: index.php');
+    exit;
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin - CRM</title>
+</head>
+<style>
+  body{
+      margin: 0;
+      padding: 0;
+  }
+  .navbar{
+      padding: 0;
+      margin: 0;
+      width: 100%;
+      height: 70px;
+      background-color: lightslategray;
+  }
+
+.non-link{
+color: black;
+cursor: default;
+}
+.navbar a{
+text-decoration: none;
+padding: 5px 10px;
+}
+.nav-left{
+width: fit-content;
+float: left;
+}
+.nav-right{
+width: fit-content;
+float: right;
+}
+.nav-left,.nav-right{
+padding: 25px 10px;
+display: inline-block;
+}
+#lgout{
+background-color: darkslategrey;
+color: aliceblue;
+border-radius: 5px;
+}
+#lgout:hover{
+opacity: 0.8;
+}
+</style>
+<body>
+    <div class="navbar"><div class="nav-left"><a href="javascript: void(0)" class="non-link">Admin - CRM</a></div><div class="nav-right"><a href="javascript: void(0)" class="non-link"><?php echo $_SESSION['user'] ?></a><a id="lgout" href="logout.php">Logout</a></div></div>
+    
+</body>
+</html>
