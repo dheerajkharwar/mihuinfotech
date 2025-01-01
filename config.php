f7013b3da029d5c02e6192fdc03eb70213d28d52
dheerajkharwar <dheerajkharwar9@gmail.com>
2025-01-01 07:35:05 +0530

    festival img

--- config.php
+++ config.php
@@ -1,5 +1,10 @@
 <?php
-$currentURL = "https://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+$currentURL = "";
+if (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on') {
+    $currentURL = "https://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+} else {
+    $currentURL = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+}
 function generateRandomString($length = 10)
 {
     return substr(str_shuffle(str_repeat($x = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ', ceil($length / strlen($x)))), 1, $length);
