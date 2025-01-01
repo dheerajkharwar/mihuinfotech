824df364d61024a8f2b7f36f6a262ce8a73d2c23
dheerajkharwar <dheerajkharwar9@gmail.com>
2025-01-01 08:19:32 +0530

    festival img

--- config.php
+++ config.php
@@ -1,11 +1,16 @@
 <?php
 $currentURL = "";
+$base = '';
 if (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on') {
     $currentURL = "https://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+    $base_url = "https://mihusystems.in/";
 } else {
     $currentURL = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+    $base_url = "http://localhost/mihusystems/";
 }
 function generateRandomString($length = 10)
 {
     return substr(str_shuffle(str_repeat($x = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ', ceil($length / strlen($x)))), 1, $length);
 }
+?>
+<base href="<?= $base ?>">
