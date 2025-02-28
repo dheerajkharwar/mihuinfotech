0b96023ee4c6fd0a60d6841e6bd8233080da5915
Dheeraj Kharwar <100955259+dheerajkharwar@users.noreply.github.com>
2025-02-28 09:15:48 +0530

    optimization performed

--- index.php
+++ index.php
@@ -1,30 +1,32 @@
 <?php
 include('components/header.php');
-if (!isset($_GET['page'])) {
-    include('home.php');
-} elseif ($_GET['page'] == 'home') {
-    include('home.php');
-} elseif ($_GET['page'] == 'about') {
-    include('about.php');
-} elseif ($_GET['page'] == 'career') {
-    include('career.php');
-} elseif ($_GET['page'] == 'contact') {
-    include('contact.php');
-} elseif ($_GET['page'] == 'portfolio') {
-    include('portfolio.php');
-} elseif ($_GET['page'] == 'services') {
-    include('services.php');
-} elseif ($_GET['page'] == 'ship-policy') {
-    include('ship-policy.php');
-} elseif ($_GET['page'] == 'privacy-policy') {
-    include('privacy-policy.php');
-} elseif ($_GET['page'] == 'rc') {
-    include('rc.php');
-} elseif ($_GET['page'] == 'tc') {
-    include('tc.php');
-} elseif ($_GET['page'] == 'team') {
-    include('team.php');
+if (isset($_GET['page'])) {
+    if ($_GET['page'] == 'home') {
+        include('home.php');
+    } elseif ($_GET['page'] == 'about') {
+        include('about.php');
+    } elseif ($_GET['page'] == 'career') {
+        include('career.php');
+    } elseif ($_GET['page'] == 'contact') {
+        include('contact.php');
+    } elseif ($_GET['page'] == 'portfolio') {
+        include('portfolio.php');
+    } elseif ($_GET['page'] == 'services') {
+        include('services.php');
+    } elseif ($_GET['page'] == 'ship-policy') {
+        include('ship-policy.php');
+    } elseif ($_GET['page'] == 'privacy-policy') {
+        include('privacy-policy.php');
+    } elseif ($_GET['page'] == 'rc') {
+        include('rc.php');
+    } elseif ($_GET['page'] == 'tc') {
+        include('tc.php');
+    } elseif ($_GET['page'] == 'team') {
+        include('team.php');
+    } else {
+        header('Location: 404.php');
+    }
 } else {
-    header('Location: 404.php');
+    include('home.php');
 }
 include("components/footer.php");
